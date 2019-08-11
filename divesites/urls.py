00ec364a724968974af014e6f43818f08a0ec817
tom reilly <treilly94@live.com>
2019-08-11 19:02:31 +0100

    Added divesites views

--- divesites/urls.py
+++ divesites/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
+app_name = 'divesites'
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail')
 ]
--- divesites/views.py
+++ divesites/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Location
+
+
+class IndexView(generic.ListView):
+    template_name = 'divesites/index.html'
+    context_object_name = 'latest_location_list'
+
+    def get_queryset(self):
+        """
+        Return the all locations alphabetically
+        """
+        return Location.objects.order_by('location_name')
+
+
+class DetailView(generic.DetailView):
+    model = Location
+    template_name = 'divesites/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Location.objects
