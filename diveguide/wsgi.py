d3b25928bd848b4cc14aad94c3dbb9e0096d5527
tom reilly <treilly94@live.com>
2019-08-24 16:47:50 +0100

    Updated wsgi to apply to multiple environments

--- diveguide/wsgi.py
+++ diveguide/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diveguide.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diveguide.settings." + os.environ['ENV'])
 
 application = get_wsgi_application()
