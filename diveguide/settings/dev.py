32568c1df03f99de21a6cbe9e3ef66ed59620e87
tom reilly <treilly94@live.com>
2019-08-25 18:34:24 +0100

    Set debug to false in dev.py

--- diveguide/settings/dev.py
+++ diveguide/settings/dev.py
@@ -12,7 +12,7 @@ from .base import *
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
